c7a16d13d506ed2d5730583111b3c6d3aa855de9
Marios <mariospatsis@gmail.com>
2020-02-29 18:33:44 +0200

    fix custom hook

--- src/shared/useForm.js
+++ src/shared/useForm.js
@@ -19,6 +19,7 @@ const useForm = (callback, validate) => {
     };
 
     const handleChange = (event) => {
+        event.persist();
         setValues(values => ({ ...values, [event.target.name]: event.target.value }));
     };
 
