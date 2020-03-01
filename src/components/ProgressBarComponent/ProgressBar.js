26fbee6a32f6d2caf73607731decaeaf43af9411
Marios <mariospatsis@gmail.com>
2020-03-01 15:53:08 +0200

    color on bars

--- src/components/ProgressBarComponent/ProgressBar.js
+++ src/components/ProgressBarComponent/ProgressBar.js
@@ -4,7 +4,9 @@ import "./ProgressBar.css";
 const ProgressBar = ({index,amount}) =>{
     return(
         <div className="wrapper">
-            <div className="progress-bar-box"></div>
+            <div className="progress-bar-box">
+                <div className={`color${index}`} style={{width: `${amount/10}%`, height: "100%"}}></div>
+            </div>
             <img src={`assets/icons/${index}.png`} className="icon" style={{left: `${amount/10}%`}} alt={"icon"}/>
         </div>
     )
